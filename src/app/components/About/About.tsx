ffd01a5466e3137c8c9fba4e5768e42a3b5d5f89
i.kalou <ibrahim.200263@gmail.com>
2025-01-28 15:58:17 +0300

    add Footer component

--- src/app/components/About/About.tsx
+++ src/app/components/About/About.tsx
@@ -11,11 +11,11 @@ const About = () => {
 
   return (
     <div className="text-center justify-center">
-      <h1 className="text-primary text-center text-3xl font-bold mb-14 underLine relative mx-auto w-16 m-16">حول</h1>
+      <h1 className="text-primary font-bold text-2xl underLine relative w-12 text-center mx-auto m-16">حول</h1>
       <p className="text-center justify-center w-8/12 mx-auto mb-8 text-lg text-muted" dir="rtl">تطبيقنا هو الحل الأمثل لإدارة معاملاتك المالية بسهولة وأمان يتيح لك إرسال واستلام الأموال بسرعة وسلاسة، مع واجهة استخدام بسيطة وتجربة مريحة نسعى من خلاله إلى تقديم خدمات مالية مبتكرة تضمن الشفافية والموثوقية، مما يتيح لك التحكم الكامل بأموالك في أي وقت ومن أي مكان.</p>
       <Aboutcard links={links} />
     </div>
   )
 }
 
-export default About
+export default About;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import FAQ from "./_components/FAQ";
 import Landing from "./_components/Landing";
 import About from "./components/About/About";
+import Footer from "./components/Footer/Footer";
 
 export default function Home() {
   return (
     <div className="">
       <Landing />
       <About/>
-      <FAQ />
+      <FAQ/>
+      <Footer/>
     </div>
   );
 }
