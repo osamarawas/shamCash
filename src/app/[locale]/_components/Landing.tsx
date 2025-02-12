1143f0866615c29b88455e28e519f891f506d97c
ahmad.khayat <ahmad.khayat@globe-co.com>
2025-02-12 09:43:14 +0300

    edit  btn download

--- src/app/[locale]/_components/Landing.tsx
+++ src/app/[locale]/_components/Landing.tsx
@@ -7,10 +7,10 @@ const Landing = async () => {
   return (
     <div
       className="h-screen
-      flex justify-center items-center lg:space-y-4 text-center mx-auto overflow-hidden bg-landing
+      flex justify-center items-center text-center mx-auto overflow-hidden bg-landing
       bg-no-repeat bg-[url(../assets/images/landing-light.png)] dark:bg-[url(../assets/images/landing-dark.png)]"
     >
-      <div className="space-y-4">
+      <div className="">
         {/* المحتوى */}
         <h1 className="text-2xl lg:text-4xl font-bold leading-tight">
           <span className="text-gradiant text-transparent bg-clip-text">
@@ -18,13 +18,13 @@ const Landing = async () => {
           </span>
           {t("landing.appAvalable")}
         </h1>
-        <h2 className="text-xl font-semibold leading-9 ">
+        <h2 className="text-xl font-semibold leading-9 mt-4 ">
           {t("landing.downloadApp")}
         </h2>
-        <p className="w-3/5 mx-auto font-medium text-lg text-gray-600 leading-relaxed">
+        <p className="w-3/5 mx-auto font-medium text-lg text-gray-600 leading-relaxed mt-4">
           {t("landing.platformGoal")}
         </p>
-        <div>
+        <div className="flex justify-center mt-16">
           <DownloadBtn />
           {/* <Button
             className="py-2 px-12 mt-8 text-white bg-blue-500 hover:bg-blue-600 rounded-lg shadow-lg"  >
