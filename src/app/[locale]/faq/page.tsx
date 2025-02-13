6c59ddbbf5b6d24331e830ffd92f4e3cec16e8b0
Osama <mylearncourses70@gmail.com>
2025-02-13 15:37:40 +0300

    add search input

--- src/app/[locale]/faq/page.tsx
+++ src/app/[locale]/faq/page.tsx
@@ -1,62 +1,50 @@
 import { faqCategories } from "@/app/utils/siteData";
 import Category from "./_components/Category";
 import { getTranslations } from "next-intl/server";
+import { Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { setDirction } from "@/app/utils/helperServer";
+import { Languages } from "@/app/utils/enums";
+interface FaqPageProps {
+  params: Promise<{ locale: Languages }>;
+}
 
-const FaqPage = async () => {
-  const t = await getTranslations("faqPage");
+const FaqPage = async ({ params }: FaqPageProps) => {
+  const { locale } = await params;
+
+  const t = await getTranslations("");
   const faqCategoriesArray = await faqCategories();
 
   return (
     <div className="flex items-center justify-center container flex-col gap-5 mx-auto  my-8 lg:my-14 ">
       <h2 className="-z-10 text-primary text-center text-3xl font-bold mb-14 underLine relative mx-auto  ">
-        {t("title")}
+        {t("faqPage.title")}
       </h2>
       <p className="w-2/3 text-center  text-lg text-muted leading-9 font-medium ">
-        {t("description")}
+        {t("faqPage.description")}
       </p>
-      <div className="">
-        <div className="w-80  mx-auto">
-          <form>
-            <label
-              htmlFor="default-search"
-              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-            >
-              Search
-            </label>
-            <div className="relative">
-              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                <svg
-                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                  />
-                </svg>
-              </div>
-              <input
-                type="search"
-                id="default-search"
-                className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Search Mockups, Logos..."
-                required
-              />
-              <button
-                type="submit"
-                className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Search
-              </button>
+
+      <div className="w-80  mx-auto">
+        <form>
+          <label
+            htmlFor="default-search"
+            className="mb-2 text-sm font-medium  sr-only "
+          >
+            {t("search")}
+          </label>
+          <div className="relative" dir={setDirction(locale)}>
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <Search size={20} className="text-muted" />
             </div>
-          </form>
-        </div>
+
+            <Input
+              type="search"
+              id="default-search"
+              placeholder={t("search")}
+              className=" block p-4 ps-10 "
+            />
+          </div>
+        </form>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2  text-center gap-6">
         {faqCategoriesArray.map((category) => (
