daeaff5b0d98e72d22fdd24958b56fd18dbf17c2
Osama <mylearncourses70@gmail.com>
2025-02-20 09:42:42 +0300

    edit landign style

--- src/app/[locale]/_components/Landing.tsx
+++ src/app/[locale]/_components/Landing.tsx
@@ -1,8 +1,10 @@
 import DownloadBtn from "@/app/components/DownloadBtn";
-import { getTranslations } from "next-intl/server";
+import { Languages } from "@/app/utils/enums";
+import { getLocale, getTranslations } from "next-intl/server";
 
 const Landing = async () => {
   const t = await getTranslations("");
+  const locale = await getLocale();
 
   return (
     <div
@@ -11,15 +13,19 @@ const Landing = async () => {
       bg-no-repeat bg-[url(../assets/images/landing-light.png)] dark:bg-[url(../assets/images/landing-dark.png)]"
     >
       <div className="">
-        {/* المحتوى */}
         <h1 className="text-2xl lg:text-4xl font-bold leading-tight">
+          {locale === Languages.ARABIC && t("landing.app") + " "}
           <span className="text-gradiant ">{t("landing.appName") + " "}</span>
+          {locale === Languages.ENGLISH && t("landing.app") + " "}
           {t("landing.appAvalable")}
         </h1>
         <h2 className="text-xl font-semibold leading-9 mt-4 ">
           {t("landing.downloadApp")}
         </h2>
-        <p className="md:w-3/5 mx-auto font-medium text-lg text-muted leading-relaxed mt-4" dir="auto">
+        <p
+          className="md:w-3/5 mx-auto font-medium text-lg text-muted leading-relaxed mt-4"
+          dir="auto"
+        >
           {t("landing.platformGoal")}
         </p>
         <div className="flex justify-center mt-16">
