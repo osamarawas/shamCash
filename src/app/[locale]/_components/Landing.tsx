ed5774bca0744c478334651b24e9410e1b9387ea
ahmad.khayat <ahmad.khayat@globe-co.com>
2025-03-06 10:56:22 +0300

    edit app word

--- src/app/[locale]/_components/Landing.tsx
+++ src/app/[locale]/_components/Landing.tsx
@@ -16,7 +16,6 @@ const Landing = async () => {
       <div className="">
         <h1 className="text-2xl lg:text-4xl font-bold leading-tight">
           <span className="text-gradiant ">{t("landing.appName") + " "}</span>
-          {locale === Languages.ENGLISH && t("landing.app") + " "}
           {t("landing.appAvalable")}
         </h1>
         <h2 className="text-xl font-semibold leading-9 mt-4 ">
